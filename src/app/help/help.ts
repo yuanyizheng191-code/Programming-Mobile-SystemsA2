75790219ff75acbb0710d04348161c71716dbdc6
yuanyizheng191-code <yuanyizheng191@gmail.com>
2026-04-06 15:53:37 +0800

    Delete src directory

--- src/app/help/help.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-help',
-  imports: [],
-  templateUrl: './help.html',
-  styleUrl: './help.css',
-})
-export class Help {}
