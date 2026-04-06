0e9efdd5cca6727d3d8db824ce609b7a31fbdef2
yuanyizheng191-code <yuanyizheng191@gmail.com>
2026-04-06 16:04:15 +0800

    Delete part2 directory

--- part2/src/app/privacy/privacy.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-privacy',
-  imports: [],
-  templateUrl: './privacy.html',
-  styleUrl: './privacy.css',
-})
-export class Privacy {}
